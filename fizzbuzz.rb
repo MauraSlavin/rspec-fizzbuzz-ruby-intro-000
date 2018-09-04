3a27c5c3257eeff594dbe74f390714a1b92a1e62
Maura Slavin <mauramslavin@gmail.com>
2018-09-04 04:29:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,14 @@
 def fizzbuzz(integer)
 
    if integer/15 == integer/15.0
-     answer = "FizzBuzz"
-   elseif integer/3 == integer/3.0
-      answer = "Fizz"
-   end
-end
+      answer = "FizzBuzz"
+   else 
+      if integer/3 == integer/3.0
+         answer = "Fizz"
+      else
+         if integer/5 == integer/5.0
+            answer = "Buzz"
+         end  # if 5
+      end # if 3
+   end  # if 15
+end  #Def
