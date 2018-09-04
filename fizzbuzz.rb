25f555332a18eb6bc9bc068c14ee05c92109f1c4
Maura Slavin <mauramslavin@gmail.com>
2018-09-04 04:15:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
-answer = String.new
-  if integer/3 == integer/3.0
-    answer = answer + "Fizz"
+
+   if integer/15 == integer/15.0
+     answer = "FizzBuzz" 
+   elseif integer/3 == integer/3.0
+     answer = "Fizz"
+   elseif integer/5 == integer/5.0
+     answer = "Buzz"
   end
-  if integer/5 == integer/5.0
-    answer = answer + "Buzz"
-  end
-answer
 end
