115686079112183a046746751dbb20189558e747
Maura Slavin <mauramslavin@gmail.com>
2018-09-04 04:02:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(integer)
+  
+  if number/3 == number/3.0
+    prints "Fizz"
+  end
+  if number/5 == number/5.0
+    prints "Buzz"
+  end
 
 end
